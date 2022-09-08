025a45981d80368d444aa170a39822fed2ae81a4
Lorik Abrashi <loriik.abrashi@gmail.com>
2022-09-08 11:14:40 +0200

    warnings remove

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+export const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
 export const passwordRegex = /^(?:(?=.*[a-z])(?:(?=.*[A-Z])(?=.*[\d\W])|(?=.*\W)(?=.*\d))|(?=.*\W)(?=.*[A-Z])(?=.*\d)).{6,}$/
 
 export const MENU_TYPES = {
